72d40fcc406be59b9166ca681caa4c863716479d
haris316 <gogeto931@gmail.com>
2022-08-15 21:00:59 +0500

    bug fix

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const employeeRoute = require('./routes/employee')
 const dropboxRoute = require('./routes/dropbox')
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb'}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true, limit: '50mb'}));
+app.use(express.json());
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
